265d391608287ebce22005dca7648ac0c6349eb4
fahadAziz-byte <sp23-bse-089@cuilahore.edu.pk>
2025-08-16 09:22:42 +0500

    project structure is now ready and also virtual environment is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Fahad Aziz",
+    author_email="fahadaziz714@gmail.com",
+    packages=find_packages()
+)
